ca4ea2f6e089fac3f8c0419ed411bad196de2919
Machrov254 <76179701+Machrov254@users.noreply.github.com>
2020-12-21 21:51:26 +0200

    a
    a

--- paskaita nr.8/function.js
+++ paskaita nr.8/function.js
@@ -1,18 +1,34 @@
 "use strict"
 
 
-function changeColorYellow() {
-    document
-        .getElementById('container')
-        .style.backgroundColor = 'yellow';
-}
+let arrayOfColors = [
+    'red', // index 0
+    'yellow', // index 1
+    'green', // index 2
+    'purple',
+    'black',
+]
 
-function changeColorBlue() {
-    document
-        .getElementById('container')
-        .style.backgroundColor = 'blue';
+let index = 0;
+
+/**
+ * Add width and height to function changeColor
+ */
+
+
+function changeColor() {
+    let randomLeft = Math.random() * 100;
+    let element = document
+        .getElementById('container');
+    element.style.backgroundColor = arrayOfColors[index];
+    element.style.left = randomLeft + '%';
+    console.log(arrayOfColors[index], index);
+    // index += 1;
+    index = (index + 1) % arrayOfColors.length;
 }
 
 function onLoad() {
-    changeColorRed();
+    document.getElementById('buttonChangeColor').onclick = changeColor;
 }
+
+window.onload = onLoad;
--- paskaita nr.8/logic.js
+++ paskaita nr.8/logic.js
@@ -1,5 +1,1 @@
 "use strict"
-
-let und;
-
-let arrayVar = [10, 10, 10];
